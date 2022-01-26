d2645d6a151c43f6ff8c07a0eb2c24235099f6e9
Gabriel <gabrielsantossousa774@gmail.com>
2022-01-26 13:02:02 -0300

    ver 0.1

---
+++ components/LandingPage.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react'
+import styles from '../styles/Home.module.css'
+import Image from 'next/image'
+import avatar from '../public/img/eu.jpg'
+
+export default function landingPage() {
+    const [text, setText] = useState("")
+    const [text2, setText2] = useState("")
+    const [showLine, setShowLine] = useState(false)
+
+    const writeText = () => {
+        const FullText1 = "Gabriel Santos"
+        const FullText2 = "Desenvolvedor"
+
+        let i = 0
+        const letters = setInterval(() => {
+            setText(text += FullText1[i])
+            i++
+            if (i == FullText1.length) {
+                clearInterval(letters)
+                setShowLine(true)
+                i = 0
+            }
+        }, 100)
+        setTimeout(() => {
+            const letters2 = setInterval(() => {
+                setText2(text2 += FullText2[i])
+                console.log(i)
+                i++
+                if (i == FullText2.length) clearInterval(letters2)
+            }, 100)
+        }, 1400)
+    }
+
+    useEffect(() => {
+        writeText()
+    }, [])
+
+
+    return <div className={styles.landingPage}>
+        <div className={styles.banner} />
+        <div className={styles.content}>
+            <Image height={250} width={250} src={avatar} className={styles.img} />
+            <div style={{ minHeight: '150px' }}>
+                <h1 className={styles.title}>
+                    {text}
+                    {showLine ? false : <span className={styles.line}>|</span>}
+                </h1>
+                <h3 className={styles.subtitle}>
+                    {text2}
+                    {showLine ? <span className={styles.line}>|</span> : false}
+
+                </h3>
+            </div>
+            <h4 className={styles.desc}>Adoro tecnologia desde a minha infância, com a programação  encontrei a possibilidade de transformar essa paixão em trabalho, desde então estudo diariamente para criar aplicações cada vez melhores.</h4>
+        </div>
+    </div>
+}
---
+++ pages/index.jsx
@@ -0,0 +1,39 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import LandingPage from '../components/LandingPage'
+import homeIcon from '../public/img/home.png'
+import codeIcon from '../public/img/coding.png'
+import skillsIcon from '../public/img/habilidades.png'
+import contactIcon from '../public/img/correspondencia.png'
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Gabriel Santos portfolio</title>
+        <meta name="description" content="Site de portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <nav className={styles.nav}>
+        <a className={styles.link}><Image src={homeIcon} /></a>
+        <a className={styles.link}><Image src={codeIcon} /></a>
+        <a className={styles.link}><Image src={skillsIcon} /></a>
+        <a className={styles.link}><Image src={contactIcon} /></a>
+      </nav>
+      <nav className={styles.navMobile}>
+        <a className={styles.link}><Image src={homeIcon} /></a>
+        <a className={styles.link}><Image src={codeIcon} /></a>
+        <a className={styles.link}><Image src={skillsIcon} /></a>
+        <a className={styles.link}><Image src={contactIcon} /></a>
+      </nav>
+
+      <main className={styles.main}>
+        <LandingPage />
+        <LandingPage />
+      </main>
+
+      <footer className={styles.footer}></footer>
+    </div>
+  )
+}
